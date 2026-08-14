25eb73edaa07cb5a8623ea4b2c614fdeb60a33e9
INSP18 <prestigenkoumando17@outlook.fr>
2026-08-14 04:28:25 +0100

    feat: récupération des données via API OpenWeather

---
+++ Js/main.js
@@ -0,0 +1,18 @@
+const API_KEY = "224385ee91e9eac147966dd324bb6ce9"
+async function getWeatherData(city="Brazzaville"){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=fr`)
+        
+        if(!response.ok){
+            throw new Error ("Impossible de récupérer la météo. Réessayer")
+        }
+
+        const dataWeather = await response.json()
+        console.log(dataWeather)
+
+    }catch(error){
+        console.error("Erreur de récupération", error.message )
+    }
+    
+    getWeatherData("Brazzaville")
+}
